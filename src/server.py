0f1f7b79ecfd087bc6f27570cf9e988228d8d167
Janill Lema <jl4817@columbia.edu>
2020-04-27 05:32:26 -0400

    modified the interface's home page

--- src/server.py
+++ src/server.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/", methods=['POST','GET'])
 def render_index_html():
     return render_template('home.html')
+
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ src/static/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('[lang="es"]').hide();
+    
+
+    $('#lang_link').click(function() {
+        if ($('#lang_link').text() =='ESPAÑOL'){
+            $('#lang_link').text('ENGLISH')
+        }
+        else{
+            $('#lang_link').text('ESPAÑOL')
+        }
+        $('[lang="es"]').toggle();
+        $('[lang="en"]').toggle();   
+   
+    });
+
+
+
+    
+
+
+
+});
