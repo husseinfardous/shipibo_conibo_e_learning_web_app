9adc41907a785adfa4b606f6848cc855c1ea9bb7
Janill Lema <jl4817@columbia.edu>
2020-04-26 21:57:19 -0400

    created server and html pages

--- src/server.py
+++ src/server.py
@@ -1,1 +1,7 @@
-# Flask 
+from flask import Flask
+from flask import render_template, Response, request, jsonify, redirect, url_for
+app = Flask(__name__)
+
+@app.route("/", methods=['POST','GET'])
+def render_index_html():
+    return render_template('home.html')
