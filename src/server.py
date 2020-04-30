ab0cac58100e06ed28598eeccc257862cdccc726
Janill Lema <jl4817@columbia.edu>
2020-04-30 16:53:33 -0400

    added language feature

--- src/server.py
+++ src/server.py
@@ -2,13 +2,25 @@ from flask import Flask
 from flask import render_template, Response, request, jsonify, redirect, url_for
 app = Flask(__name__)
 
-@app.route("/", methods=['POST','GET'])
-def render_index_html():
-    return render_template('home.html')
+language_setting = "ESPANOL"
 
-@app.route("/about_team", methods=['POST','GET'])
+@app.route("/", methods=['GET'])
+def render_index_html():
+    return render_template('home.html', language_setting = language_setting)
+
+@app.route("/about_team", methods=['GET'])
 def render_about_team_html():
-    return render_template('about_team.html')
+    return render_template('about_team.html', language_setting = language_setting)
+
+@app.route("/change_language", methods=['POST','GET'])
+def change_language():
+
+    global language_setting
+
+    json_data = request.get_json()
+    language_setting = json_data['new_language']
+
+    return jsonify(language_setting=language_setting)
 
 if __name__ == '__main__':
     app.run()
--- src/static/main.js
+++ src/static/main.js
@@ -1,19 +1,55 @@
+//used to change the language of the website 
+function change_language(lang_set){
+
+    var new_language = ''
+    if (lang_set === 'ESPANOL'){
+        new_language = 'ENGLISH'
+    }
+    else{
+        new_language = 'ESPANOL'
+    }
+
+    new_data = {'new_language': new_language}
+
+    $.ajax({
+        type: "POST",
+        url: "change_language",
+        dataType : "json",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(new_data),
+        success: function(result){
+            language_setting = result['language_setting']
+            if (language_setting ==='ESPANOL'){
+                $('#lang_link').text('ENGLISH')
+            }
+            else{
+                $('#lang_link').text('ESPAÑOL')
+            }
+        },
+    });
+}
 
 
 $(document).ready(function(){
 
     // webpage language
-    $('[lang="es"]').hide();
+    if (language_setting === "ESPANOL"){
+        $('[lang="en"]').hide();
+        $('#lang_link').text('ENGLISH');
+    }
+    else{
+        $('[lang="es"]').hide();
+        $('#lang_link').text('ESPAÑOL'); 
+    }
 
-    $('#lang_link').click(function() {
-        if ($('#lang_link').text() =='ESPAÑOL'){
-            $('#lang_link').text('ENGLISH')
-        }
-        else{
-            $('#lang_link').text('ESPAÑOL')
-        }
+    $('#lang_link').click(function(){
+
+        change_language(language_setting);
+
         $('[lang="es"]').toggle();
-        $('[lang="en"]').toggle();   
+        $('[lang="en"]').toggle(); 
     });
+
+
 
 });
